836f9f4d6d2fab0e266e28d3a5153d5fe42e32ea
Phil Belinsky <philbelinsky@yahoo.com>
2022-06-03 20:35:50 -0400

    first commit

---
+++ src/com/cognizant/shapes/AreaCalculator.java
@@ -0,0 +1,10 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle();
+		r1.calculateArea();
+	}
+
+}
---
+++ src/com/cognizant/shapes/Rectangle.java
@@ -0,0 +1,14 @@
+package com.cognizant.shapes;
+
+
+
+
+public class Rectangle {
+
+	
+	public void calculateArea() {
+		System.out.println("The area of the rectangle is calculated using the formula length * breadth");
+	}
+	
+	
+}
